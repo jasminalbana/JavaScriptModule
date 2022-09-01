1245e4b900bd529608e21640a6b093cc7e708676
Jasmin Albana <ramosjasminsabelita@gmail.com>
2022-09-01 03:33:00 -0600

    1st part of ES6

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 //This is the module for JavaScript, enjoy!!!!!!!!!!!!!!!!!
 
+//------------------------------------Compare Scopes of the var and let Keywords
+
+function checkScope() {
+    let i = 'love scope';
+    if (true) {
+     let i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
+
+  //checkScope();
+
+//-----------------------------------Mutate an Array Declared with const
+
+const s = [5, 7, 2];
+function editInPlace() {
+ 
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+
+}
+editInPlace();
+
