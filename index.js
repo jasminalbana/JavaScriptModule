735d2268ac7079eda022ac7e37bb655af44c1f86
Jasmin Albana <ramosjasminsabelita@gmail.com>
2022-09-09 23:47:49 -0600

    2nd part of ES6

--- index.js
+++ index.js
@@ -5,14 +5,14 @@
 function checkScope() {
     let i = 'love scope';
     if (true) {
-     let i = 'block scope';
+    // let i = 'block scope';
       console.log('Block scope i is: ', i);
     }
     console.log('Function scope i is: ', i);
     return i;
   }
 
-  //checkScope();
+//checkScope();
 
 //-----------------------------------Mutate an Array Declared with const
 
@@ -24,5 +24,47 @@ function editInPlace() {
   s[2] = 7;
 
 }
-editInPlace();
+//editInPlace();
 
+
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // Only change code below this line
+  Object.freeze(MATH_CONSTANTS)
+
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+//console.log(freezeObj());
+
+//-----------------------------------Use the Rest Parameter with Function Parameters
+
+const sum = (...args) => {
+  return args.reduce((a, b) => a + b, 0);
+}
+
+//console.log(sum(1, 2, 3))
+
+
+
+
+
+
+const bicycle = {
+  gear: 2,
+  setGear(newGear){
+    return this.gear = newGear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
