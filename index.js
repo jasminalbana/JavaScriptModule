3e5beb07f4620a51cecaf24792c45e99f1845cba
Jasmin Albana <ramosjasminsabelita@gmail.com>
2022-09-11 00:26:35 -0600

    first part of Debugging

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 //This is the module for JavaScript, enjoy!!!!!!!!!!!!!!!!!
 
+function getNine() {
+    let x = 6;
+    let y = 3;
+    return x + y;
+  }
+  
+  let result = getNine();
+ // console.log(result);
+
+
+  function myFunc() {
+    for (let i = 0; i <= 4; i += 2) {
+      console.log("Still going!");
+    }
+  }
+  console.log(myFunc());
